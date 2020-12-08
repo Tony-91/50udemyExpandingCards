5a15d1e766fe920e6f9a82664d6ce92691b06cad
Tony-91 <73248515+Tony-91@users.noreply.github.com>
2020-12-08 12:50:14 -0800

    added final notes
    experimented with more js. add / remove classes.defining the equation and its parts. "on click.. remove / add class".

--- expandingCards.js
+++ expandingCards.js
@@ -1,5 +1,8 @@
+// Define equation and its parts
+// panels = all elements with panel class
 const panels = document.querySelectorAll(".panel");
 
+// "on each click... removeActiveClass + add classList('active')"
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
     removeActiveClasses();
@@ -7,6 +10,7 @@ panels.forEach((panel) => {
   });
 });
 
+// defining what 'removeActive Class', by using a function.
 function removeActiveClasses() {
   panels.forEach((panel) => {
     panel.classList.remove("active");
